2f44bcba272fcadc9e9c5abc163c64430da85ffb
ville24 <73233385+ville24@users.noreply.github.com>
2025-03-03 19:27:37 +0000

    T3

---
+++ cli.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const { Sequelize, QueryTypes } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const main = async () => {
+  try {
+    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT, logging: false })
+    blogs.forEach(blog => {
+        console.log(blog.author + ': \'' + blog.title + '\', ' + blog.likes + ' likes')
+    })
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+main()

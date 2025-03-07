9d74031d1d9b9ef0d3beb2a98e8ff9f8618f17ff
ville24 <73233385+ville24@users.noreply.github.com>
2025-03-07 16:42:54 +0200

    T17

--- controllers/authors.js
+++ controllers/authors.js
@@ -4,7 +4,6 @@ const { Sequelize } = require('sequelize')
 const { Blog } = require('../models')
 
 router.get('/', async (req, res) => {
-  console.log(Sequelize)
     const blogs = await Blog.findAll({
       attributes: ['author', [Sequelize.fn('COUNT', Sequelize.col('id')), 'blogs'], [Sequelize.fn('SUM', Sequelize.col('likes')), 'likes']],
       group: 'author',
@@ -16,5 +15,3 @@ router.get('/', async (req, res) => {
 })
 
 module.exports = router
-
-//  SELECT "author", count("id") AS "blogs", sum("likes") AS "likes" FROM "blogs" GROUP BY "author";
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,25 +1,7 @@
 const router = require('express').Router()
 const { Op } = require('sequelize')
 
-const jwt = require('jsonwebtoken')
-const { SECRET } = require('../util/config')
-
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      console.log(authorization.substring(7))
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-    } catch (error){
-      console.log(error)
-      return res.status(401).json({ error: 'token invalid' })
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' })
-  }
-  next()
-}
+const { tokenExtractor } = require('../util/middleware')
 
 const { Blog, User } = require('../models')
 
